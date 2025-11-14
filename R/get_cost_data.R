141787b3f5845b39f6ee142e954cf7d075cc33e4
thibautjombart <thibautjombart@gmail.com>
2025-11-14 16:42:12 +0100

    adding costs estimates and lower/upper bounds

--- R/get_cost_data.R
+++ R/get_cost_data.R
@@ -30,3 +30,74 @@ get_cost_category <- function(x) {
   names(res) <- paste("cost_category", 1:10, sep = "_")
   res
 }
+
+
+
+
+#' Get costs estimates
+#' 
+#' This processes the 10 columns containing cost information for each row of the
+#' original database.
+#' 
+#' @inheritParams get_cost_category
+#' 
+get_cost_estimate <- function(x) {
+  
+  cols_to_keep <- grep(
+    "cost_estimate_or_mean_cost_",
+    names(x),
+    value = TRUE
+  )
+  
+  out <- x[, cols_to_keep]
+  names(out) <- paste("cost_estimate", seq_along(cols_to_keep), sep = "_")
+  out
+}
+
+
+
+
+#' Get costs lower bound
+#' 
+#' This processes the 10 columns containing cost information for each row of the
+#' original database.
+#' 
+#' @inheritParams get_cost_category
+#' 
+get_cost_lower <- function(x) {
+  
+  cols_to_keep <- grep(
+    "cost_range_lower_bound_",
+    names(x),
+    value = TRUE
+  )
+  
+  out <- x[, cols_to_keep]
+  names(out) <- paste("cost_lower", seq_along(cols_to_keep), sep = "_")
+  out
+}
+
+
+
+
+#' Get costs upper bound
+#' 
+#' This processes the 10 columns containing cost information for each row of the
+#' original database.
+#' 
+#' @inheritParams get_cost_category
+#' 
+get_cost_upper <- function(x) {
+  
+  cols_to_keep <- grep(
+    "cost_range_upper_bound_",
+    names(x),
+    value = TRUE
+  )
+  
+  out <- x[, cols_to_keep]
+  names(out) <- paste("cost_upper", seq_along(cols_to_keep), sep = "_")
+  out
+}
+
+
