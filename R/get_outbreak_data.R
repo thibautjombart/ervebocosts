436522ac93afb83a9288b3c5ba231347f07b56ab
thibautjombart <thibautjombart@gmail.com>
2025-11-14 13:18:57 +0100

    get_outbreak_data done

--- R/get_outbreak_data.R
+++ R/get_outbreak_data.R
@@ -1,16 +1,8 @@
-#' Get pathogen information
-#' 
-#' @param x the imported database as returned by [import_data]
-get_pathogen <- function(x) {
-  select(x, "pathogen")
-}
-
-
 #' Get country information
 #' 
 #' This can be one or more countries.
 #' 
-#' @inheritParams get_pathogen
+#' @param x the imported database as returned by [import_data]
 #' @param concatenate a `logical` indicating if several countries should be 
 #' collapsed into a single character string
 
@@ -35,9 +27,12 @@ get_locations <- function(x, concatenate = FALSE) {
 }
 
 
+
+
+
 #' Get starting date of outbreak
 #' 
-#' @inheritParams get_pathogen
+#' @inheritParams get_locations
 
 get_start_date <- function(x) {
   year <- x$outbreak_start_year_outbreak_1
@@ -49,9 +44,10 @@ get_start_date <- function(x) {
 
 
 
+  
 #' Get ending date of outbreak
 #' 
-#' @inheritParams get_pathogen
+#' @inheritParams get_locations
 
 get_end_date <- function(x) {
   year <- x$outbreak_end_year_outbreak_1
@@ -61,3 +57,25 @@ get_end_date <- function(x) {
 }
 
 
+
+
+
+#' Get outbreak data
+#' 
+#' @inheritParams get_locations
+
+get_outbreak_data <- function(x) {
+  tibble(
+    pathogen = x$pathogen,
+    locations = get_locations(x, TRUE), 
+    start_date = get_start_date(x),
+    end_date = get_end_date(x),
+    cases = x$number_of_confirmed_cases_outbreak_1,
+    deaths = x$number_of_deaths_outbreak_1,
+    sdbs = x$number_of_safe_burials_outbreak_1,
+    vaccinations = x$number_of_vaccines_administered_outbreak_1,
+    tests = x$number_of_tests_carried_out_outbreak_1,
+    admissions = x$number_of_etu_admissions_outbreak_1,
+    contacts_traced = x$number_contacts_traced_outbreak_1
+  )
+}
