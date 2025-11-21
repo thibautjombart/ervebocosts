29000b80487e247003bb4428766ac078eafd4e8c
cm401 <christian.morgenstern@gmail.com>
2025-11-21 16:58:08 +0000

    New file to classify what type of cost this is (fixed, fixed variable or variable).
    Also check if a cost item is relevant for the perspective of the analysis or not.
    
    These two functions are required to produce the cost overview table for the report.

---
+++ R/classify_costs.R
@@ -0,0 +1,58 @@
+
+
+#' Title classify_cost
+#'
+#' @param x tibble with all processed raw data from RedCap
+#'
+#' @returns vector of cost classifications into Fixed, Fixed variable, Variable or Unknown
+#' @export
+#'
+#' @examples 
+classify_cost <- function(x){
+  cost_class <- rep(NA, nrow(x))
+  
+  for(i in 1:nrow(x)){
+    cost_type <- x$cost_type[i]
+    num_act   <- x$number_activities[i]
+    
+    ## PLACEHOLDER ONLY: JS to fill in logic
+    
+    if(!is.na(cost_type) & cost_type == "Unit cost"){
+      cost_class[i] <- "Variable"
+    } else if(!is.na(num_act) & num_act > 1){
+      cost_class[i] <- "Fixed Variable"
+    } else if(!is.na(num_act) & num_act == 1){
+      cost_class[i] <- "Fixed"
+    } else {
+      cost_class[i] <- "Unknown"
+    }
+  }
+  
+  return(cost_class)
+}
+
+#' Title cost_in_perspective
+#'
+#' @param x tibble with all processed raw data from RedCap
+#'
+#' @returns vector of zero or one if the cost item is in the perspective of the analysis
+#' @export
+#'
+#' @examples
+cost_in_perspective <- function(x){
+  perspective <- rep(1, nrow(x))
+  
+  for(i in 1:nrow(x)){
+    cost_cat <- x$cost_subcategory[i]
+    
+    ## PLACEHOLDER ONLY: JS to fill in logic
+    
+    if(!is.na(cost_cat) & grepl("NHGDP Loss", cost_cat)){
+      perspective[i] <- 0
+    } else if(!is.na(cost_cat) & grepl("GDP per Capita Loss", cost_cat)){
+      perspective[i] <- 0
+    } 
+  }
+  
+  return(perspective)
+}
